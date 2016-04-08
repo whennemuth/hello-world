544eac5cdc09d4e855277b1df39cdd65c83e8ed2
Hennemuth <wrh@bu.edu>
2016-04-07 20:02:00 -0400

    Modifying last script added

--- docker/build.context/helloworld/tomcatInit.sh
+++ docker/build.context/helloworld/tomcatInit.sh
@@ -3,6 +3,8 @@
 # Namely, copying war file from mounted directory to webapps dir so containter can start with last built war running.
 
 # Change permission level so that ssh agent does not complain about the ssh key being "too open".
-cp /usr/local/tomcat/webapp_mount/. /usr/local/tomcat/webapps/ -R
+if [ -f /usr/local/tomcat/webapp_mount/hello-world-0.0.1-SNAPSHOT.war ] ; then
+   cp -f /usr/local/tomcat/webapp_mount/hello-world-0.0.1-SNAPSHOT.war /usr/local/tomcat/webapps/hello-world.war
+fi
 
 sh /usr/local/tomcat/bin/catalina.sh "run"
