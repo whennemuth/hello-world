91c709b3827d3e42de3705ff2c06350ed8d3bc36
Hennemuth <wrh@bu.edu>
2016-04-21 15:16:28 -0400

    Updating jenkins config.xml file

--- docker/build.context/jenkins/jenkinsInit.sh
+++ docker/build.context/jenkins/jenkinsInit.sh
@@ -18,8 +18,11 @@ fi
 
 # Change permission level so that ssh agent does not complain about the ssh key being "too open".
 cp /var/jenkins_ssh_mount/. /var/jenkins_ssh/ -R
+cp /var/jenkins_ssh_mount/. /var/jenkins_home/.ssh/ -R
+
 chgrp users /var/jenkins_ssh -R
 chmod 700 /var/jenkins_ssh
 chmod 600 /var/jenkins_ssh/*
+chmod 600 /var/jenkins_home/.ssh/*
 
 source jenkins.sh
