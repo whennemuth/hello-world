79ed7640d9f7710b265d6ea6cd2c603248b3ca60
Hennemuth <wrh@bu.edu>
2016-04-27 18:33:36 -0400

    Fixing mistakes to if statement in .sh file and disabling hostname hashing

--- docker/build.context/jenkins/jenkinsInit.sh
+++ docker/build.context/jenkins/jenkinsInit.sh
@@ -5,13 +5,13 @@
 # Adding the various hosts to the known_hosts file should have been done as a RUN instruction in the DockerFile.
 # However test if this is true and fix the situation if it is not.
 if [ -f /root/.ssh/ssh_hosts ] ; then
-   if [ -n /root/.ssh/known_hosts ] || [ -n "$(cat /root/.ssh/known_hosts | grep github.com)" ] ; then 
+   if [ -! -f /root/.ssh/known_hosts ] || [ -z "$(cat /root/.ssh/known_hosts | grep github.com)" ] ; then 
       ssh-keyscan -t rsa -f /root/.ssh/ssh_hosts >> /root/.ssh/known_hosts
    fi
 fi
 
 if [ -f /var/jenkins_ssh/ssh_hosts ] ; then
-   if [ -n /var/jenkins_ssh_mount/known_hosts ] || [ -n "$(cat /var/jenkins_ssh_mount/known_hosts | grep github.com)" ] ; then 
+   if [ ! -f /var/jenkins_ssh_mount/known_hosts ] || [ -z "$(cat /var/jenkins_ssh_mount/known_hosts | grep github.com)" ] ; then 
       ssh-keyscan -t rsa -f /var/jenkins_ssh/ssh_hosts >> /var/jenkins_ssh_mount/known_hosts
    fi
 fi
