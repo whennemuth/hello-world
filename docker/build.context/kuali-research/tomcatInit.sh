d77786d92fe1012488f0f881aada9441e26cb7e7
Hennemuth <wrh@bu.edu>
2016-04-28 13:49:59 -0400

    Adding kc-config.xml handling to docker file and tomcat init script

--- docker/build.context/kuali-research/tomcatInit.sh
+++ docker/build.context/kuali-research/tomcatInit.sh
@@ -2,9 +2,15 @@
 # This script will run catalina.sh, but only after it does some custom tasks first.
 # Namely, copying war file from mounted directory to webapps dir so container can start with last built war running.
 
-# Change permission level so that ssh agent does not complain about the ssh key being "too open".
+# Copy war file from mounted directory to non-mounted webapps dir.
 if [ -f /usr/local/tomcat/webapp_mount/kc.war ] ; then
    cp -f /usr/local/tomcat/webapp_mount/kc.war /usr/local/tomcat/webapps/kc.war
+fi
+
+# Copy kc-config.xml file to appropriate location in user dir
+if [ -f /usr/local/tomcat/webapp_mount/kc-config.xml ] ; then
+   cp -f /usr/local/tomcat/webapp_mount/kc-config.xml ~/kuali/main/dev/kc-config.xml
+   cp -f /usr/local/tomcat/webapp_mount/kc-config.xml ~/kuali/main/config/kc-config.xml
 fi
 
 sh /usr/local/tomcat/bin/catalina.sh "run"
