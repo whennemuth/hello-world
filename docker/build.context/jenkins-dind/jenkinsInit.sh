9a417577a7a360a2ff82c75592e0827520cc279c
Hennemuth <wrh@bu.edu>
2016-05-02 17:08:44 -0400

    Removing backup file

--- docker/build.context/jenkins-dind/jenkinsInit.sh
+++ docker/build.context/jenkins-dind/jenkinsInit.sh
@@ -11,21 +11,21 @@ if [ -f /root/.ssh/ssh_hosts ] ; then
 fi
 
 if [ -f /var/jenkins_ssh/ssh_hosts ] ; then
-   if [ ! -f /var/jenkins_ssh_mount/known_hosts ] || [ -z "$(cat /var/jenkins_ssh_mount/known_hosts | grep github.com)" ] ; then 
-      ssh-keyscan -t rsa -f /var/jenkins_ssh/ssh_hosts >> /var/jenkins_ssh_mount/known_hosts
+   if [ ! -f /var/lib/jenkins_ssh_mount/known_hosts ] || [ -z "$(cat /var/lib/jenkins_ssh_mount/known_hosts | grep github.com)" ] ; then 
+      ssh-keyscan -t rsa -f /var/lib/jenkins_ssh/ssh_hosts >> /var/lib/jenkins_ssh_mount/known_hosts
    fi
 fi
 
 # make .ssh dir (-p flag will avoid error if dir already exists).
 # Then copy the mounted ssh content to the non-mounted ssh dir.
-mkdir -p /var/jenkins_home/.ssh
-cp /var/jenkins_ssh_mount/. /var/jenkins_ssh/ -R
-cp /var/jenkins_ssh_mount/. /var/jenkins_home/.ssh/ -R
+mkdir -p /var/lib/jenkins_home/.ssh
+cp /var/lib/jenkins_ssh_mount/. /var/lib/jenkins_ssh/ -R
+cp /var/lib/jenkins_ssh_mount/. /var/lib/jenkins_home/.ssh/ -R
 
 # Change permission level so that ssh agent does not complain about the ssh key being "too open".
-chgrp users /var/jenkins_ssh -R
-chmod 700 /var/jenkins_ssh
-chmod 600 /var/jenkins_ssh/*
-chmod 600 /var/jenkins_home/.ssh/*
+chgrp users /var/lib/jenkins_ssh -R
+chmod 700 /var/lib/jenkins_ssh
+chmod 600 /var/lib/jenkins_ssh/*
+chmod 600 /var/lib/jenkins_home/.ssh/*
 
 source jenkins.sh
