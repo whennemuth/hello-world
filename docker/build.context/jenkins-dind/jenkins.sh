aba072f32a6f5495aaf3dca54e4026325b7a2d59
Hennemuth <wrh@bu.edu>
2016-05-03 14:19:52 -0400

    Adding jenkins user to first supervisord.conf entry

--- docker/build.context/jenkins-dind/jenkins.sh
+++ docker/build.context/jenkins-dind/jenkins.sh
@@ -58,7 +58,7 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
   echo "Jenkins launcher arguments present: JENKINS_OPTS = $JENKINS_OPTS, JAVA_OPTS = $JAVA_OPTS"
-  eval "exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
+  eval "su jenkins && exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\" && su root"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
