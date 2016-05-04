4ec8f2905ac200c615461423a2f2a1d62cb98275
Hennemuth <wrh@bu.edu>
2016-05-04 12:26:21 -0400

    Adding redirect of stdout to log file

--- docker/build.context/jenkins-dind/jenkins.sh
+++ docker/build.context/jenkins-dind/jenkins.sh
@@ -1,7 +1,7 @@
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  echo "Jenkins launcher arguments present: JENKINS_OPTS = $JENKINS_OPTS, JAVA_OPTS = $JAVA_OPTS"
-  eval "su jenkins && exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\" && su root"
+  echo "Jenkins launcher arguments present: JENKINS_OPTS = $JENKINS_OPTS, JAVA_OPTS = $JAVA_OPTS" >> /var/log/jenkins/stdout-supervisor.log
+  eval "exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
