b96aaa636170bf966379315464bb8adcc13bbdf6
Hennemuth <wrh@bu.edu>
2016-05-11 13:05:57 -0400

    Adding new build context dir for helloworld-dind

--- docker/build.context/helloworld/tomcatInit.sh
+++ docker/build.context/helloworld/tomcatInit.sh
@@ -2,7 +2,6 @@
 # This script will run catalina.sh, but only after it does some custom tasks first.
 # Namely, copying war file from mounted directory to webapps dir so containter can start with last built war running.
 
-# Change permission level so that ssh agent does not complain about the ssh key being "too open".
 if [ -f /usr/local/tomcat/webapp_mount/hello-world-0.0.1-SNAPSHOT.war ] ; then
    cp -f /usr/local/tomcat/webapp_mount/hello-world-0.0.1-SNAPSHOT.war /usr/local/tomcat/webapps/hello-world.war
 fi
